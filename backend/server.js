f57c3b29787857238f02db72232879c2945d0e72
nishith-02 <sainishith02.varaganti@gmail.com>
2021-07-06 08:15:12 +0530

    database

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
 import express from 'express'
-import data from './data.js'
+import mongoose from 'mongoose'
+import productRouter from './routers/productRouter.js'
+import userRouter from './routers/userRouter.js'
 
 const app=express()
+mongoose.connect('mongodb+srv://ecommerce:ecommerce123@cluster0.qccee.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-app.get('/api/products',(req,res)=>{
-    res.send(data.products)
-})
 app.get('/',(req,res)=>{
     res.send('Server is ready')
 })
-app.get('/api/products/:id',(req,res)=>{
-    const product=data.products.find(x=>x._id===req.params.id)
-    if(product){
-        res.send(product)
-    }
-    else{
-        res.status(404).send({message:'Product Not Found'})
-    }
+
+
+app.use('/api/users',userRouter)
+app.use('/api/products',productRouter)
+
+app.use((err,req,res,next)=>{
+    res.status(500).send({message:err.message})
 })
+
 const port=process.env.PORT||5000
 app.listen(port,()=>{
     console.log('Serve at port 5000')
